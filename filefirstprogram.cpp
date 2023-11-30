f1150180d2667c96b63150842f85a5fbbd47a615
Woafi <woafisun@yahoo.com>
2023-11-30 10:04:31 +0600

    update 2

--- filefirstprogram.cpp
+++ filefirstprogram.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 
 using namespace std;
 
 int main(){
-    string st ="Woafi have done it";
-    string st3 ="\n2nd line";
+    string st;
+    cout<<"Enter a Text: ";
+    getline(cin, st);
+    string st3;
+    cout<<"Enter 2nd Text: ";
+    getline(cin, st3);
 
     //opening files using constructor
     ofstream out("firstfileprogram.txt");
     //write operation
     out<<st;
+    out<<"\n";
     out<<st3;
 
-    string st2;
+    /*string st2;
     //opening files using constructor
     ifstream in("firstfileprogram.txt");
     //read operation
     //in>>st2; for reading a first word from file
     getline(in, st2); //for firstline of file
     //getline(in, st2); //for 2nd line of file //after 2nd line of execution 1st line will be vanish
-    cout<<st2;
+    cout<<st2;*/
     
     return 0;
 }
