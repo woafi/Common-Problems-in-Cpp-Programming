bd7fea96cbe80d53632af542fa20ef313454eac0
Mohammad Woafi <woafisun@yahoo.com>
2024-02-09 00:22:31 +0600

    New File

--- magicSquare.cpp
+++ magicSquare.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 //magic Square Algorithm
 void magic(int n){
     if (n % 2 == 0){
@@ -8,28 +9,25 @@ void magic(int n){
     } else{
         for (int i=0; i<n-1; i++){
             for (int j=0; j<n-1; j++){
-                int square[i][j] = 0;
+                int square[i][j];
                 square[0][(n-1)/2] = 1;
                 j =(n-1)/2;
-                for (int key=2; key<(n*n); key++){
+                for (int key=2; key<=(n*n); key++){
                     int k,l;
                     if (i>=1){k=i-1;} else {k=n-1;}
-                    if (j>=1){l=j-1} else {l=n-1;}
-                    if (sqaure[k][l] >= 1) {i=(i+1)%n;
-                    }else {
+                    if (j>=1){l=j-1;} else {l=n-1;}
+                    if (square[k][l] >= 1) {i=(i+1)%n;}
+                    else {
                         i=k;
                         j=l;
                     }
                     square[i][j]=key;
                 }
-            }
-        }
-        for (int i=0; i<n-1; i++){
-            for (int j=0; j<n-1; j++){
                 cout<<square[i][j]<<" ";
             }
             cout<<endl;
         }
+        return;
     }
 }
 
