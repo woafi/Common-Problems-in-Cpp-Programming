20fdc43d28767a3dd26d39752e9d58c776c4d195
Mohammad Woafi <woafisun@yahoo.com>
2024-02-16 12:56:23 +0600

    new file

---
+++ Array/pointerArray.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+    int arr[] = {24, 55, 66};
+    int* p = arr;
+    cout<< *p<<endl;
+    cout<< *(p+1)<<endl;
+    cout<< *(p+2)<<endl;
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 
-
 using namespace std;
 
+int partition(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = low-1;
+    for (int j=low; j<high; j++){
+        if (arr[j] < pivot){
+            i++;
+            int temp = arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    
+}
 
+quickSort(int arr[], int low, int high){
+    if (low < high){
+        int pidex = partition(arr, low, high);
+    
+    quickSort(arr, low, pidex-1);
+    quickSort(arr, pidex+1, high);
+    }
+}
 
 int main(){
-   int max = Integer.MIN_VALUE;
-    return 0;
+    int arr[]={6,3,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr, 0, n-1);
+    for (int i=0; i<n; i++){
+       cout<<arr[i]<<" ";
+    }
+    return 0; 
 }
